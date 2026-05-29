c1123759289055c1e1f92d5d063a0595758e7b3c
mohotsarkar <sarkarmohot@gmail.com>
2026-05-29 22:22:45 +0600

    payoo login form

---
+++ script/javascript.js
@@ -0,0 +1,15 @@
+
+document.getElementById("login-btn").addEventListener('click', function(){
+    const numberInput = document.getElementById("input-number");
+    const contactNumber =numberInput.value;
+    const pinNumber = document.getElementById("pin-number");
+    const pin = pinNumber.value;
+    if(contactNumber == "01311675428" && pin == "1234"){
+        alert("login Successful");
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("login Failed");
+        return;
+    }
+} )
